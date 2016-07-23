b506c7a65b11fe66ac2c7b4f92b9be746f6266d9
lon90053 <lon9005@naver.com>
2016-07-23 17:58:41 +0900

    커밋

--- app/src/main/java/com/example/a1027/weekendapp/member/JoinActivity.java
+++ app/src/main/java/com/example/a1027/weekendapp/member/JoinActivity.java
@@ -1,15 +1,67 @@
 package com.example.a1027.weekendapp.member;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.a1027.weekendapp.MainActivity;
 import com.example.a1027.weekendapp.R;
 
-public class JoinActivity extends Activity {
-
+public class JoinActivity extends Activity implements View.OnClickListener  {
+    EditText etID,etPW,etName,etEmail,etPhone,etPhoto,etAddr;
+    Button btJoin,btHome;
+    TextView tvResult;
+    MemberBean bean = new MemberBean();
+    MemberService service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        service = new MemberServiceImpl(this);
+        etID = (EditText) findViewById(R.id.etID);
+        etPW = (EditText) findViewById(R.id.etPW);
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        etPhoto = (EditText) findViewById(R.id.etPhoto);
+        etAddr = (EditText) findViewById(R.id.etAddr);
+        btJoin = (Button) findViewById(R.id.btJoin);
+        btHome = (Button) findViewById(R.id.btHome);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        btJoin.setOnClickListener(this);
+        btHome.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String id = etID.getText().toString();
+        String pw = etPW.getText().toString();
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+        String phone = etPhone.getText().toString();
+        String photo = etPhoto.getText().toString();
+        String addr = etAddr.getText().toString();
+        bean.setId(id);
+        bean.setPw(pw);
+        bean.setName(name);
+        bean.setEmail(email);
+        bean.setPhone(phone);
+        bean.setPhoto(photo);
+        bean.setAddr(addr);
+        switch (v.getId()){
+            case R.id.btJoin:
+                service.join(bean);
+                this.startActivity(new Intent(this,LoginActivity.class));
+                break;
+            case R.id.btHome:
+                this.startActivity(new Intent(this,MainActivity.class));
+                break;
+            default : break;
+        }
     }
 }
--- app/src/main/java/com/example/a1027/weekendapp/member/LoginActivity.java
+++ app/src/main/java/com/example/a1027/weekendapp/member/LoginActivity.java
@@ -2,14 +2,45 @@ package com.example.a1027.weekendapp.member;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a1027.weekendapp.R;
 
-public class LoginActivity extends Activity {
 
+public class LoginActivity extends Activity implements View.OnClickListener {
+    EditText etID,etPW;
+    TextView textResult;
+    Button btLogin;
+    MemberService service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        service = new MemberServiceImpl(this.getApplicationContext());
+        etID = (EditText) findViewById(R.id.etID);
+        etPW = (EditText) findViewById(R.id.etPW);
+        btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String id = etID.getText().toString();
+        String pw = etPW.getText().toString();
+        Log.d("로그인 액티비티 ID",id);
+        Log.d("로그인 액티비티 PW",pw);
+        MemberBean member = new MemberBean();
+        member.setId(id);
+        member.setPw(pw);
+        if(service.login(member)){
+            Toast.makeText(LoginActivity.this,"로그인성공",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(LoginActivity.this,"로그인실패",Toast.LENGTH_SHORT).show();
+        }
     }
 }
